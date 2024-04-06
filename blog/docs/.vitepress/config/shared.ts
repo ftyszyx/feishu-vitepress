c496d3cc33d15801e059e323750a634b104fc7ed
ftyszyx <whyzi@qq.com>
2024-04-06 20:43:41 +0800

    logo

--- blog/docs/.vitepress/config/shared.ts
+++ blog/docs/.vitepress/config/shared.ts
@@ -21,7 +21,7 @@ export const shared = defineConfig({
       {},
       '(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "lqpzet81i6");',
     ],
-    ["link", { rel: "icon", href: " / logo.png" }],
+    ["link", { rel: "icon", href: "/logo.png" }],
     [
       "meta",
       {
