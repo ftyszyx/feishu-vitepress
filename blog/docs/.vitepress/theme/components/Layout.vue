f8b314d9993fb8e6b39a723573cce0d7926e921e
ftyszyx <whyzi@qq.com>
2024-04-20 20:38:19 +0800

    fix bug

--- blog/docs/.vitepress/theme/components/Layout.vue
+++ blog/docs/.vitepress/theme/components/Layout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, watch, nextTick } from "vue";
 import DefaultTheme from "vitepress/theme";
-import { useRoute, useRouter } from "vitepress";
+import { useRouter } from "vitepress";
 import mediumZoom from "medium-zoom";
 import CategoryNav from "./CategoryNav.vue";
 import ArticleMeta from "./ArticleMeta.vue";
--- blog/docs/index.md
+++ blog/docs/index.md
@@ -4,5 +4,4 @@ title: 首页
 hide: true
 hide_child: false
 create_time: 1711551874
-cover: /normal_cover.png
 ---
