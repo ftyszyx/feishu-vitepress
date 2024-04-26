a08a537aea18f97310318b927d2cff0809f70927
zyx <whyzi@qq.com>
2024-04-26 11:47:18 +0800

    change umami

--- blog/docs/.vitepress/theme/site_config.ts
+++ blog/docs/.vitepress/theme/site_config.ts
@@ -23,12 +23,18 @@ export const SiteConfig = {
     site: "myblog",
     server: "https://arktalk_blog.qiancizhan.com/",
   },
+  //lcoal test
   // umami_url: "http://localhost:3000",
   // umami_script: "http://localhost:3000/script.js",
   // umami_website_id: "132ba91a-f8e4-4a19-b316-20b6267923ad",
+  //cloudflare
+  // umami_url: "https://umami_blog.qiancizhan.com",
+  // umami_script: "https://umami_blog.qiancizhan.com/script.js",
+  // umami_website_id: "8cf20240-e118-4863-8665-598549bec563",
+  //bytefuse
   umami_url: "https://umami_blog.qiancizhan.com",
   umami_script: "https://umami_blog.qiancizhan.com/script.js",
-  umami_website_id: "8cf20240-e118-4863-8665-598549bec563",
+  umami_website_id: "8186755e-1054-407b-8f79-51d9692ff4f2",
   github_url: "https://github.com/ftyszyx",
   twitter_url: "https://twitter.com/zhangyuxin_new",
 };
