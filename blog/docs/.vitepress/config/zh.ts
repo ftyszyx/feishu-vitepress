6f478238a4cdd8a13385ac603a1f0bdf083dfbd2
ftyszyx <whyzi@qq.com>
2024-05-16 22:43:19 +0800

    change site title

--- blog/docs/.vitepress/config/zh.ts
+++ blog/docs/.vitepress/config/zh.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitepress";
 import sidebar from "../../sider.json";
 export const zh = defineConfig({
-  title: "小白看世界",
+  title: "超级小朋友日常",
   lang: "zh-Hans",
   description: "个人博客",
 
